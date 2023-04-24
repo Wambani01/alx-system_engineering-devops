24713e512adda8193b1f5edb8611320bcabcf953
Wambani01 <georgewmbn@gmail.com>
2023-04-24 04:59:22 -0700

    processing json data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,8 +5,7 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1].strip()
-    print(type(user_id))
+    user_id = argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
     user_dict = requests.get(user_url).json()
     user_name = user_dict.get("name")
