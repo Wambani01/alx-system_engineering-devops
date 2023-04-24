59ab7fb7c8436f1b12a614b204d96a8063e16402
Wambani01 <georgewmbn@gmail.com>
2023-04-24 06:10:02 -0700

    writing to csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     user_id = argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/" + user_id
     user_dict = requests.get(user_url).json()
-    user_name = user_dict.get("name")
+    user_name = user_dict.get("username")
     user_todo = requests.get("{}/todos".format(user_url))
     user_todo = user_todo.json()
     file_name = user_id + ".csv"
